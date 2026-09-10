4ce56c802c34b600751f6342956f936a9592998d
FNTDUG <henryl.darner@gmail.com>
2026-09-10 14:03:15 -0500

    Rename 'Vengeful Spirit' to 'Vengeful Spirit P1'

--- tierlist.js
+++ tierlist.js
@@ -20,7 +20,7 @@
         "Gaia Chica",
         "Cyberdata Withered Bonnie NEW",
         "Kronos Endo Freddy",
-        "Vengeful Spirit",
+        "Vengeful Spirit P1",
         "Deep Sea Calamity Endo",
         "Void Shaper Shadow Freddy",
         "DJ 101Bit",
